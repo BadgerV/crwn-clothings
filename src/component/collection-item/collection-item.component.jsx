a5a62ba1f802215b6a21c24df3bcf148923a2705
Faozan Segunmaru <segunfaozan@gmail.com>
2022-02-25 13:32:33 -0800

    continued with the shopping project

--- src/component/collection-item/collection-item.component.jsx
+++ src/component/collection-item/collection-item.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import {connect} from 'react-redux';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+import {addItem} from '../../redux/cart/cart.actions'
 
-    console.log(`url${imageUrl}`)
+import CustomButton from '../custom-button/custom-button.component'
+
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl} = item;
     return(
         <div className = 'collection-item'>
         <div className = 'image'
@@ -15,10 +20,18 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
                 <span className = 'name'>{name}</span>
                 <span className = 'price'>{price}</span>
             </div>
+
+
+            <CustomButton onClick = {() => addItem(item)}  inverted>Add to cart</CustomButton>
             
         
     </div>
     )
 }
 
-export default CollectionItem;
+
+    const mapDispatchToProps = (dispatch) => ({
+        addItem : item => dispatch(addItem(item))
+    })
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
