268c808fca42510f6a84fe4ea49515fa5b0dfb9d
Faozan Segunmaru <segunfaozan@gmail.com>
2022-02-25 23:43:44 -0800

    continued with some changes

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,6 @@
-import CartActionTypes from './cart.types'
+import CartActionTypes from './cart.types';
+
+import {addItemToCart} from './cart.utils'  
 
 const INITIAL_STATE = {
     hidden : true,
@@ -18,7 +20,7 @@ const CartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM :
             return {
                 ...state,
-                cartItems : [...state.cartItems, action.payload]
+                cartItems : addItemToCart(state.cartItems, action.payload)
             }
         default :
         return state;
