c5ec198a5e28dc83537b6dbdd5b56c7313fba65a
Faozan Segunmaru <segunfaozan@gmail.com>
2022-03-04 11:33:23 -0800

    added remove item functionality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,5 @@
+import CartItem from "../../component/cart-item/cart-item";
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
@@ -8,4 +10,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
 
     return [...cartItems, {...cartItemToAdd, quantity : 1}]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+    
+    
+    return cartItems.map (cartItem => 
+        cartItem.id === cartItemToRemove.id ? {...cartItem, quantity : cartItem.quantity - 1} : 
+        cartItem
+        );
 }
