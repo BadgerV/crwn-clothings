86ab9e90c58e3e083ac2f8f1835351118e111c90
Faozan Segunmaru <segunfaozan@gmail.com>
2022-03-08 08:44:28 -0800

    continued to imporve tha app

--- src/pages/collection-preview/collection-preview.component.jsx
+++ src/pages/collection-preview/collection-preview.component.jsx
@@ -4,7 +4,7 @@ import CollectionItem from '../../component/collection-item/collection-item.comp
 
 import './collection-preview.styles.scss'
 
-const PreviewCollection = ({title, items}) => (
+const CollectionPreview = ({title, items}) => (
     <div className = 'collection-preview'>
         <h1 className = 'title'>{title.toUpperCase()}</h1>
         <div className = 'preview'>
@@ -18,4 +18,4 @@ const PreviewCollection = ({title, items}) => (
 )
 
 
-export default PreviewCollection;
+export default CollectionPreview;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 
-import {connect} from 'react-redux'
+import {Route} from 'react-router-dom'
+import CollectionsOverview from '../../component/collections-overview/collections-overview.component'
 
-import {createStructuredSelector} from 'reselect'
+import CollectionPage from '../collection/collection.component';
 
-import {selectCollections} from '../../redux/shop/shop.selectors'
 
-import CollectionPreview from '../collection-preview/collection-preview.component'
-
-const ShopPage= ({collections}) => (
+const ShopPage= ({match}) => (
     
             <div className = 'shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key = {id} {...otherCollectionProps} />
-                    ))
-                }
+                <Route exact path = {`${match.path}`} component = {CollectionsOverview} />
+                <Route path = {`${match.path}/:collectionId`} component = {CollectionPage} />
             </div>
         )
 
 
 
 
-const mapStateToProps = createStructuredSelector({
-    collections : selectCollections
-})
-export default connect(mapStateToProps)(ShopPage)
+
+export default ShopPage;
